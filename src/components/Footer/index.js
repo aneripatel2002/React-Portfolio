cda7e18318d137da97cab406b358b0b90cb7d094
Aneri Patel <aneripatel2002@gmail.com>
2024-07-31 23:44:34 -0400

    added code

---
+++ src/components/Footer/index.js
@@ -0,0 +1,40 @@
+import React from "react";
+import {
+  FaGithub,
+  FaLinkedin,
+  FaBehanceSquare,
+  FaInstagram,
+} from "react-icons/fa";
+
+function Footer() {
+  return (
+    <div className="footer-div">
+      <footer>
+        <ul>
+          <a href="https://github.com/aneripatel2002" target="blank">
+            <li>
+              <FaGithub />
+            </li>
+          </a>
+          <a href="https://www.linkedin.com" target="blank">
+            <li>
+              <FaLinkedin />
+            </li>
+          </a>
+          <a href="https://www.behance.net" target="blank">
+            <li>
+              <FaBehanceSquare />
+            </li>
+          </a>
+          <a href="https://www.instagram.com" target="blank">
+            <li>
+              <FaInstagram />
+            </li>
+          </a>
+        </ul>
+      </footer>
+    </div>
+  );
+}
+
+export default Footer;
---
+++ src/components/Header/index.js
@@ -0,0 +1,18 @@
+import React from "react";
+import { Link } from "react-router-dom";
+import NavBar from "../NavBar";
+
+function Header() {
+  return (
+    <header className="flex-row px-1">
+      <div className="logo">
+        <h1>
+          <Link to="/">Aneri Patel</Link>
+        </h1>
+      </div>
+      <NavBar></NavBar>
+    </header>
+  );
+}
+
+export default Header;
