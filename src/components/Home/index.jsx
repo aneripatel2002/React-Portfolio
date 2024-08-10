6c999110728356c90382215c9dd840209e522406
Aneri Patel <aneripatel2002@gmail.com>
2024-08-10 02:09:52 -0400

    fixing profile pic

--- src/components/Home/index.jsx
+++ src/components/Home/index.jsx
@@ -1,6 +1,6 @@
 import React from "react";
 import { Link } from "react-router-dom";
-import Portrait from "../../assets/images/profile_pic.jpg";
+import Portrait from "../../assets/images/profile.jpg";
 
 function Home() {
   return (
